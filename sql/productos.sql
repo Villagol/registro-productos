db61eee26b1ed872ccbcb03f3ac5b132db1d3d78
Villagol <mar.villablanca2@gmail.com>
2025-09-04 20:09:27 -0400

    esquema sql

--- sql/productos.sql
+++ sql/productos.sql
@@ -1,44 +1,47 @@
---Eliminar tablas para evitar errores
-DROP TABLE IF EXISTS producto CASCADE;
-DROP TABLE IF EXISTS sucursal CASCADE;
-DROP TABLE IF EXISTS bodega CASCADE;
-DROP TABLE IF EXISTS moneda CASCADE;
---Crear tablas
-CREATE TABLE bodega (
+-- Crear esquema
+CREATE SCHEMA inventario;
+
+-- Crear tablas
+DROP TABLE IF EXISTS inventario.producto CASCADE;
+DROP TABLE IF EXISTS inventario.sucursal CASCADE;
+DROP TABLE IF EXISTS inventario.bodega CASCADE;
+DROP TABLE IF EXISTS inventario.moneda CASCADE;
+
+CREATE TABLE inventario.bodega (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE sucursal (
+CREATE TABLE inventario.sucursal (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
-    bodega_id INT REFERENCES bodega(id)
+    bodega_id INT REFERENCES inventario.bodega(id)
 );
 
-CREATE TABLE moneda (
+CREATE TABLE inventario.moneda (
     id SERIAL PRIMARY KEY,
     codigo VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE producto (
+CREATE TABLE inventario.producto (
     id SERIAL PRIMARY KEY,
     codigo VARCHAR(15) UNIQUE NOT NULL,
     nombre VARCHAR(50) NOT NULL,
-    bodega_id INT REFERENCES bodega(id),
-    sucursal_id INT REFERENCES sucursal(id),
-    moneda_id INT REFERENCES moneda(id),
+    bodega_id INT REFERENCES inventario.bodega(id),
+    sucursal_id INT REFERENCES inventario.sucursal(id),
+    moneda_id INT REFERENCES inventario.moneda(id),
     precio NUMERIC(10,2) NOT NULL,
     material VARCHAR(100) NOT NULL,
     descripcion TEXT NOT NULL
 );
 
 -- Insertar datos
-INSERT INTO bodega (nombre) VALUES
+INSERT INTO inventario.bodega (nombre) VALUES
 ('Bodega 1'),
 ('Bodega 2'),
 ('Bodega 3');
 
-INSERT INTO sucursal (nombre, bodega_id) VALUES
+INSERT INTO inventario.sucursal (nombre, bodega_id) VALUES
 ('Sucursal 1A', 1),
 ('Sucursal 1B', 1),
 ('Sucursal 1C', 1),
@@ -51,11 +54,12 @@ INSERT INTO sucursal (nombre, bodega_id) VALUES
 ('Sucursal 3B', 3),
 ('Sucursal 3C', 3);
 
-INSERT INTO moneda (codigo) VALUES
+INSERT INTO inventario.moneda (codigo) VALUES
 ('CLP'),
 ('USD'),
 ('EUR'),
 ('GBP'),
 ('JPY');
---Ver Productos
-SELECT * FROM producto
+
+-- Ver Productos
+SELECT * FROM inventario.producto;
